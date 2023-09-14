42b41c65c37153dbe672b8260f42dbb6874eca93
jdBritto <jefdabri2011@gmail.com>
2023-09-14 07:37:48 -0300

    ADherencia de React Proyect

--- React/primer-proyecto-react/src/PrimariComponent.jsx
+++ React/primer-proyecto-react/src/PrimariComponent.jsx
@@ -1,3 +1,5 @@
+import './css/PrimariComponent.css'
+
 const string = 'Esto es un texto'
 const number = 123456
 const array = ['Curso de React', 'Youtube', 4, 100000]
@@ -7,16 +9,14 @@ const objeto = { nombre: ' Curso de Javascript', duracion: 4 }
 const fecha = new Date()
 
 
-export const PrimariComponent = () => {
+
+export const PrimariComponent = ( {titulo, subtitulo} ) => {
+  console.log(titulo)
+  console.log(subtitulo)
   return (
     <>
-      <h1>Variables en JSX</h1>
-      <h4>Variable tipo String:</h4> <p>{string}</p>
-      <h4>Variable tipo number:</h4> <p>{number}</p>
-      <h4>Variable tipo array:</h4> <p>{array}</p>
-      <h4>Variable tipo boolean:</h4> <p>{boolean}</p>
-      <h4>Variable tipo funcion:</h4> <p>{funcion()}</p>
-      <h4>Variable tipo String:</h4> <p>{string}</p>
+      <h1>{titulo} </h1>
+      <h2>{subtitulo}</h2>
     </>
   )
 }
--- React/primer-proyecto-react/src/main.jsx
+++ React/primer-proyecto-react/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PrimariComponent } from './PrimariComponent'
+import './css/normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimariComponent />
+    <PrimariComponent titulo="Esta session es de titulo" subtitulo="Curso de react"/>
   </React.StrictMode>,
 )
+
