1a7d2801d07b8985194f8396a48b43fd96c6d35e
jefferson britto castro <jefdabri2011@gmail.com>
2024-06-04 18:01:07 -0300

    Studen js 2

---
+++ 9.JavaScript/IntroduccionJS/js/script_07.js
@@ -0,0 +1,11 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;
+
+resultado = (100 + 200 + 300) * .2;
+
+
+
+console.log(resultado);
---
+++ 9.JavaScript/IntroduccionJS/js/script_14.js
@@ -0,0 +1,4 @@
+// Arrays
+
+const numeros = [20,30,40,50,60,70,80,90]
+console.table(numeros);
